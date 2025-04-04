557a29a92c87faaf03290fc606d1706433c69d62
michalknapikdcu <michal.knapik2@mail.dcu.ie>
2025-04-05 00:27:33 +0200

    RNN data picker in progress

--- RNNs/build_route_database.py
+++ RNNs/build_route_database.py
@@ -6,11 +6,14 @@ import random
 import haversine
 import pandas as pd
 
-def prepare_data(dataset_path, length_bottom_threshold, length_top_threshold):
+def prepare_data(dataset_path, length_bottom_threshold, length_top_threshold, \
+                 sample_row_count, minimal_ride_req):
 
+    # read and summarize data
     print('** Reading dataset...')
     db = pd.read_csv(dataset_path)
     print(f'** Done. Read {len(db)} rows.\n')
+    print(f'** Printing columns with their datatypes:\n{db.dtypes}\n')
     print(f'** Printing the header:\n{db.head()}\n')
 
     # drop columns decided irrelevant for the assignment
@@ -27,39 +30,71 @@ def prepare_data(dataset_path, length_bottom_threshold, length_top_threshold):
     print(f'** Dropping NA values, if any: {len(db)} rows left.\n')
 
     # prepare a series with total route lengths and drop from database the rows with 
-    # routes too long or too short
-    route_lengths = db.apply(lambda row: len(row.POLYLINE), axis=1)
+    # routes too long or too short; 
+    # there is a trick here: these entries are Pandas objects, i.e. strings; so to find
+    # the length of the sequence of coordinates like '[[1.2, 2.3], [1.23, 4.55], ...]'
+    # it suffices to count '[' and subtract 1
+    route_lengths = db.apply(lambda row: row.POLYLINE.count('[') - 1, axis=1)
     route_length_discriminator = ((route_lengths >= length_top_threshold) | \
                                   (route_lengths <= length_bottom_threshold))
     print(f'** Dropping {sum(route_length_discriminator)} rows with '\
           f'routes shorter than {length_bottom_threshold} or longer than '\
           f'{length_top_threshold}.')
-
     db.drop(db.index[route_length_discriminator], inplace=True)
-    print(f'** {len(db)} rows left.')
+    print(f'** {len(db)} rows left.\n')
 
+    # drop rows with taxi drivers whose activity is below the given threshold
+    taxi_id_count = len(db.TAXI_ID.unique())
+    rides_count = db.groupby('TAXI_ID').size()
+    active_taxi_ids = rides_count[rides_count >= minimal_ride_req]
+    active_taxi_count = len(active_taxi_ids)
+    inactive_driver_count = taxi_id_count - active_taxi_count
+    inactive_taxi_discriminator = db.apply(lambda row: row['TAXI_ID'] not in active_taxi_ids, axis=1) 
+    print(f'** Dropping all rows with taxi ids who have less than {minimal_ride_req} rides.')
+    print(f'** There are {inactive_driver_count} of {taxi_id_count} such taxis.')
+    db.drop(db.index[inactive_taxi_discriminator], inplace=True)
+    print(f'** {len(db)} rows left.\n')
+
+    # now, sample the same number of rows for each taxi id, to get approximately 
+    # the requested number of rows in total, evenly distributed over ids
+    group_sample_count = sample_row_count//active_taxi_count
+    print(db.groupby('TAXI_ID').sample(n=group_sample_count).count())
+ 
+    return
+    # compute distances between adjacent steps in routes using haversine formula
+    def get_distances(polyline):
+        print(list(zip(polyline[1:], polyline[:-1])))
+        print([haversine.haversine(p, n, unit=haversine.Unit.METERS) \
+                for p,n in zip(polyline[1:], polyline[:-1])])
+
+    adj_distances = db.apply(lambda row: get_distances(row.POLYLINE), axis=1)
+    print(adj_distances)
+   
     # TODO - filter out bad routes based on haversine lengths
     # TODO - normalize GPS data?
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 6:
+    if len(sys.argv) < 7:
         print(f'Usage: {sys.argv[0]} dataset_path datapoints_to_sample '\
-              'jump_threshold length_bottom_threshold length_top_threshold')
+              'jump_threshold length_bottom_threshold length_top_threshold '\
+              'minimal_ride_req')
         sys.exit(1)
 
     dataset_path = sys.argv[1] # the path to the data file 
-    sample_row_count = int(sys.argv[2]) # the number of rows to produce 
+    sample_row_count = int(sys.argv[2]) # the approximate number of rows to produce 
     jump_threshold = float(sys.argv[3]) # the maximal valid distance between two adjacent GPS coordinates
-    length_bottom_threshold = float(sys.argv[4]) # the minimal valid length of a route 
-    length_top_threshold = float(sys.argv[5]) # the maximal valid length of a route 
-
+    length_bottom_threshold = float(sys.argv[4]) # the minimal valid number of steps in a route 
+    length_top_threshold = float(sys.argv[5]) # the maximal valid number of steps in a route 
+    minimal_ride_req = int(sys.argv[6]) # the minimal number of rides required from a taxi
 
     print('**')
     print(f'** Collecting {sample_row_count} rows with jump threshold: {jump_threshold}, ')
     print(f'** bottom and top length thresholds: {length_bottom_threshold}, {length_top_threshold}')
-    print('**')
+    print(f'** and at least {minimal_ride_req} rides required from a taxi.')
+    print('**\n')
 
-    prepare_data(dataset_path, length_bottom_threshold, length_top_threshold)
+    prepare_data(dataset_path, length_bottom_threshold, length_top_threshold, \
+                 sample_row_count, minimal_ride_req)
 
     print('** all done')
